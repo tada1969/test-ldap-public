13f79b6484ecc49e584412b74719c39d9b2e9dfd
Nipa Staff <nipa_staff@atcreative.internal>
2024-06-14 02:57:30 +0000

    Update 2 files
    - /ssh-authen-with-ldap.sh
    - /README.md

---
+++ ssh-authen-with-ldap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sed -i 's/gosaas-lma-server/gosaas-lma-server.atcreative.internal/g' /etc/hosts
+hostnamectl set-hostname $(hostnamectl hostname | cut -d. -f1).atcreative.internal
+yes '' | apt install freeipa-client -y
+ipa-client-install --unattended --mkhomedir --no-ntp --principal=$FREEIPA_USERNAME --password=$FREEIPA_PASSWORD --force-join
+sed -i '1 a default_shell = /bin/bash' /etc/sssd/sssd.conf
+sed -i '1 a override_shell = /bin/bash' /etc/sssd/sssd.conf
+systemctl restart sssd
